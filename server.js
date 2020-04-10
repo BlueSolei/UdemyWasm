2a16787c8ea76a1a0bcf9d7ec35bf0d9a735c8dc
Shaul Fridman <shaul.fridman@gmail.com>
2020-04-11 00:41:42 +0300

    Basic emscripten

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const app =  express()
+const express = require("express");
+const app = express();
 
-app.use(express.static('public'))
+const dir = `${__dirname}/public`;
+console.log(`Shaul: ${dir}`);
+app.use(express.static(dir));
 
-var port = 2222
-app.listen(port, () => console.log(`Server run on port ${port}`))
+var port = 2222;
+app.listen(port, () => console.log(`Server run on port ${port}`));
